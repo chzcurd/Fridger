6ebdd4b413d8ae1394a6bc7d755b16a7853c7824
Josh Osmanski <osman006@rangers.uwp.edu>
2022-03-17 18:45:29 -0500

    Added Camera Scanner view page, still need to add code

---
+++ Fridger/Views/ScannerView.swift
@@ -0,0 +1,20 @@
+//
+//  ScannerView.swift
+//  Fridger
+//
+//  Created by Josh Osmanski on 3/17/22.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView()
+    }
+}
