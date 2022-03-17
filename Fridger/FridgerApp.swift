7231c370311652dbd2bf01f131114106e89f3937
Josh Osmanski <osman006@rangers.uwp.edu>
2022-03-17 18:40:02 -0500

    Initial Commit

---
+++ Fridger/FridgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgerApp.swift
+//  Fridger
+//
+//  Created by App Factory on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FridgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
