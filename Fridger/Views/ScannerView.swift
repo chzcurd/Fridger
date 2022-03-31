2c9205e8ac9e320e271a12ca9bfc0986dcd8494f
Josh Osmanski <osman006@rangers.uwp.edu>
2022-03-31 14:48:40 -0500

    Got scanner working

--- Fridger/Views/ScannerView.swift
+++ Fridger/Views/ScannerView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ScannerView: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+
+            Button() {
+                isPresentingScanner = true
+            }
+            label: {
+            Label("Scan barcode", systemImage: "barcode.viewfinder")
+            }
+
+            
+            Text("Scan result:")
+            if let code = scannedCode {
+                //NavigationLink("Next page", destination: NextView(scannedCode: code), isActive: .constant(true)).hidden()
+                Text(code)
+            }
+        }
+        .sheet(isPresented: $isPresentingScanner) {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "Simulated Scan successful") { response in
+                if case let .success(result) = response {
+                    scannedCode = result.string
+                    isPresentingScanner = false
+                }
+            }
+        }
     }
 }
 
