da2bef20748b51b91d0bcd094faab45421806fee
Josh Osmanski <osman006@rangers.uwp.edu>
2022-05-04 16:13:11 -0500

    got tabview to work functionally

--- Fridger/Views/ContentView.swift
+++ Fridger/Views/ContentView.swift
@@ -12,30 +12,56 @@ struct ContentView: View {
     //@State var items : [foodOBJ] = []
     
     @EnvironmentObject var scanHandler: ScanHandler
+    @State private var selection = 1
+    
+    var handler: Binding<Int> { Binding(
+        get: { self.selection},
+        set: {
+            //if $0 == self.selection {
+            print("RESET THE TAB")
+            scanHandler.currentItem = nil
+            //}
+            self.selection = $0
+        }
+    
+    )}
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Items inside fridge: " + String(getItemCountInFridge(scannedItems: scanHandler.scannedItems)))
+            //VStack {
+                //Text("Items inside fridge: " + String(getItemCountInFridge(scannedItems: scanHandler.scannedItems)))
                 
-                List{
-                    NavigationLink(destination: ItemListView(databaseView: false, titleText: "In the Fridge").environmentObject(scanHandler)) {
-                        Text("View Fridge")
+                TabView(selection: handler){
+                    NavigationView{
+                    ItemListView(databaseView: false, titleText: "In the Fridge").environmentObject(scanHandler)
                     }
-                    
-                    NavigationLink(destination: ScannerView().environmentObject(scanHandler)) {
-                        Text("Scan Item")
+                        .tabItem() {
+                            Image(systemName: "slider.horizontal.3")
+                            Text("View Fridge")
                     }
-                    NavigationLink(destination: ItemListView(databaseView: true, titleText: "Food Database").environmentObject(scanHandler)) {
-                        Text("View Database")
+                        .tag(1)
+                    NavigationView{
+                    ScannerView().environmentObject(scanHandler)
                     }
+                        .tabItem() {
+                            Image(systemName: "barcode.viewfinder")
+                            Text("Scan Item")
+                    }
+                        .tag(2)
+                    NavigationView{
+                    ItemListView(databaseView: true, titleText: "Food Database").environmentObject(scanHandler)
+                    }
+                        .tabItem() {
+                            Image(systemName: "slider.horizontal.3")
+                            Text("View Database")
+                        }.tag(3)
                     
                     
                 }
-            }
-            .navigationBarTitle(Text("Fridger"))
-            .onAppear(perform: {scanHandler.currentItem = nil})
-        }
+                .onChange(of: selection, perform: {index in (scanHandler.currentItem = nil)})
+            //}
+            //.navigationBarTitle(Text("Fridger"))
+            //.onAppear(perform: {scanHandler.currentItem = nil})
+        
         .navigationViewStyle(StackNavigationViewStyle())
         
         
