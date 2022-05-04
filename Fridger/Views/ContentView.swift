8267b70782fff9f764e92c86fd38282a74e8edd7
Josh Osmanski <osman006@rangers.uwp.edu>
2022-05-04 17:28:15 -0500

    formatted code to look nice and readable

--- Fridger/Views/ContentView.swift
+++ Fridger/Views/ContentView.swift
@@ -23,59 +23,59 @@ struct ContentView: View {
             //}
             self.selection = $0
         }
-    
+        
     )}
     
     var body: some View {
-            //Fridger App title text
-            VStack {
-                HStack{
-                    Text("Fridger").font(.largeTitle).bold().fontWeight(.black).padding(.top)
-                    VStack{
-                        Text("").padding(.top)
-                        Text("What's in the fridge?").font(.caption).fontWeight(.light)
-                    }
+        //Fridger App title text
+        VStack {
+            HStack{
+                Text("Fridger").font(.largeTitle).bold().fontWeight(.black).padding(.top)
+                VStack{
+                    Text("").padding(.top)
+                    Text("What's in the fridge?").font(.caption).fontWeight(.light)
                 }
-                TabView(selection: handler){
-                    NavigationView{
-                    ItemListView(databaseView: false, titleText: "In the Fridge").environmentObject(scanHandler)
-                    }
-                        .tabItem() {
-                            Image(systemName: "fork.knife")
-                            Text("View Fridge")
-                    }
-                        .tag(1)
-                    NavigationView{
-                    ScannerView().environmentObject(scanHandler)
-                    }
-                        .tabItem() {
-                            Image(systemName: "barcode.viewfinder")
-                            Text("Item Scanner")
-                    }
-                        .tag(2)
-                    NavigationView{
-                    ItemListView(databaseView: true, titleText: "Food Database").environmentObject(scanHandler)
-                    }
-                        .tabItem() {
-                            Image(systemName: "server.rack")
-                            Text("View Database")
-                    }
-                        .tag(3)
-                    NavigationView{
-                    settingsView().environmentObject(scanHandler)
-                    }
-                        .tabItem() {
-                            Image(systemName: "slider.horizontal.3")
-                            Text("Settings")
-                    }
-                        .tag(4)
-                    
-                    
-                }
-                .onChange(of: selection, perform: {index in (scanHandler.currentItem = nil)})
             }
-            //.navigationBarTitle(Text("Fridger"))
-            //.onAppear(perform: {scanHandler.currentItem = nil})
+            TabView(selection: handler){
+                NavigationView{
+                    ItemListView(databaseView: false, titleText: "In the Fridge").environmentObject(scanHandler)
+                }
+                .tabItem() {
+                    Image(systemName: "fork.knife")
+                    Text("View Fridge")
+                }
+                .tag(1)
+                NavigationView{
+                    ScannerView().environmentObject(scanHandler)
+                }
+                .tabItem() {
+                    Image(systemName: "barcode.viewfinder")
+                    Text("Item Scanner")
+                }
+                .tag(2)
+                NavigationView{
+                    ItemListView(databaseView: true, titleText: "Food Database").environmentObject(scanHandler)
+                }
+                .tabItem() {
+                    Image(systemName: "server.rack")
+                    Text("View Database")
+                }
+                .tag(3)
+                NavigationView{
+                    settingsView().environmentObject(scanHandler)
+                }
+                .tabItem() {
+                    Image(systemName: "slider.horizontal.3")
+                    Text("Settings")
+                }
+                .tag(4)
+                
+                
+            }
+            .onChange(of: selection, perform: {index in (scanHandler.currentItem = nil)})
+        }
+        //.navigationBarTitle(Text("Fridger"))
+        //.onAppear(perform: {scanHandler.currentItem = nil})
         
         .navigationViewStyle(StackNavigationViewStyle())
         
